21ca868126a8dbff6759c14def0a58db12e9e992
Daniel Costa <dancosta.id@gmail.com>
2022-04-14 08:37:13 -0300

    fixing index for gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client' // react 18 supported
 // import { render } from "react-dom" ==> Not supported on react 18
+import { createRoot } from 'react-dom/client' // react 18 supported
 import { App } from './App'
 
 const container = document.getElementById('root')
