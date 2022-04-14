a1c06b53a0cac354623584d77e2cb6c1a76f82e6
Daniel Costa <dancosta.id@gmail.com>
2022-04-13 21:31:30 -0300

    I've created an app where I go through all my github repos. This was created following Rocketseat React course.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { RepositoryList } from './components/RepositoryList'
+import './styles/global.scss'
+
+export function App() {
+  // throw new Error('Eita! Something deu ruim there!')
+  return (
+    <>    
+      <RepositoryList />
+    </>
+  )
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client' // react 18 supported
+// import { render } from "react-dom" ==> Not supported on react 18
+import { App } from './App'
+
+const container = document.getElementById('root')
+const root = createRoot(container) // createRoot(container!) if you use 
+
+root.render(<App tab="home" />)
