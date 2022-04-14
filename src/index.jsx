a0751117fbe0fe061a905326a4d5e0bc08a3667a
Daniel Costa <dancosta.id@gmail.com>
2022-04-14 09:50:42 -0300

    Updates

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-// import { render } from "react-dom" ==> Not supported on react 18
-import { createRoot } from 'react-dom/client' // react 18 supported
-import { App } from './App'
-
-const container = document.getElementById('root')
-const root = createRoot(container) // createRoot(container!) if you use 
-
-root.render(<App tab="home" />)
